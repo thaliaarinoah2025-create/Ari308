ca3f20b7008a35f73d47d65d645653898d3f19ef
thaliaarinoah2025-create <thalia.ari.noah2025@gmail.com>
2025-11-20 18:19:05 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+Col = 0
+Row = 0
+basic.show_icon(IconNames.GHOST)
+for index in range(4):
+    if input.sound_level() > 128:
+        Row = 104
+    Col = 0 + 4
---
+++ main.ts
@@ -0,0 +1,9 @@
+let Col = 0
+let Row = 0
+basic.showIcon(IconNames.Ghost)
+for (let index = 0; index < 4; index++) {
+    if (input.soundLevel() > 128) {
+        Row = 104
+    }
+    Col = 0 + 4
+}
